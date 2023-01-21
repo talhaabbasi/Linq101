20058953e1a2c7add6bd6bb3fbd103aeb18baae6
Talha Abbasi <talhaabbasi2009@hotmail.com>
2023-01-21 20:43:38 +0500

    Add project files.

---
+++ LinqLibrary/ListMananger.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqLibrary
+{
+    public class ListMananger
+    {
+        public static List<Person> LoadSampleDate()
+        {
+            List<Person> output = new List<Person>();
+            output.Add(new Person { FirstName = "Talha", LastName = "Abbasi 1", DateOfBirth = DateTime.Now, YearsExperience = 0 });
+            output.Add(new Person { FirstName = "Talha", LastName = "Abbasi 2", DateOfBirth = DateTime.Now, YearsExperience = 0 });
+            output.Add(new Person { FirstName = "Talha", LastName = "Abbasi 3", DateOfBirth = DateTime.Now, YearsExperience = 0 });
+            output.Add(new Person { FirstName = "Talha", LastName = "Abbasi 4", DateOfBirth = DateTime.Now, YearsExperience = 0 });
+            output.Add(new Person { FirstName = "Talha", LastName = "Abbasi 5", DateOfBirth = DateTime.Now, YearsExperience = 0 });
+            output.Add(new Person { FirstName = "Talha", LastName = "Abbasi 6", DateOfBirth = DateTime.Now, YearsExperience = 0 });
+
+            return output;
+        }
+
+    }
+}
---
+++ LinqLibrary/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace LinqLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearsExperience { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
